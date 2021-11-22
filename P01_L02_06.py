2c7ff68ccc6152af195d2a8d26d6c407870510b1
Guro <demensdeo@gmail.com>
2021-11-23 00:43:55 +0300

    Completed exercise 6-2

--- P01_L02_06.py
+++ P01_L02_06.py
@@ -1,5 +1,6 @@
 master = []
 demo = {'name': '', 'price': '', 'amount': '', 'measure_unit': ''}
+data_anal = {'name': [], 'price': [], 'amount': [], 'measure_unit': []}
 i = 0
 while True:
     data = input().upper()
@@ -12,4 +13,12 @@ while True:
     master.append((i, demo.copy()))
 print(master)
 
+for el in master:
+    for data in el:
+        if isinstance(data, dict):
+            for key, value in data.items():
+                data_anal[key].append(value)
 
+
+print(isinstance(demo, dict))
+print(data_anal)
