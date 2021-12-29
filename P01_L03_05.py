9e0ffee2f901f5fda32bca0afa57232cf9150d83
Guro <demensdeo@gmail.com>
2021-12-29 16:04:15 +0300

    ongoing

--- P01_L03_05.py
+++ P01_L03_05.py
@@ -1,5 +1,18 @@
-demo = '5 6 7 5 4'
+demo = '5 10 4 11 3'
 
-demo_2 = input()
+#demo_2 = input()
 
-print(demo.split(' '))
+#print(demo.split(' '))
+
+foo_sum = 0
+
+
+def foo(item):
+    global foo_sum
+    item_demo = item.split()
+    for num in item_demo:
+        foo_sum += int(num)
+    print(foo_sum)
+
+
+foo(demo)
